adc56dc2a49289e29097e53f2966f1c0d5e73bdc
foyst <benfoster88@hotmail.com>
2015-09-08 19:45:35 +0100

    Pretty formatted CEP Events for pushing to Client

--- smalldata-cep-publish-ws/src/main/java/uk/co/foyst/smalldata/cep/publisher/ws/WebSocketEventPublisher.java
+++ smalldata-cep-publish-ws/src/main/java/uk/co/foyst/smalldata/cep/publisher/ws/WebSocketEventPublisher.java
@@ -1,15 +1,21 @@
 package uk.co.foyst.smalldata.cep.publisher.ws;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import uk.co.foyst.smalldata.cep.CEPEvent;
 import uk.co.foyst.smalldata.cep.publisher.EventPublisher;
 
+import java.util.Arrays;
+
 @Component
 public class WebSocketEventPublisher extends EventPublisher {
 
     private SimpMessagingTemplate template;
+    private static final DateTimeFormatter TIMESTAMP_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss,SSS");
 
     @Autowired
     public WebSocketEventPublisher(SimpMessagingTemplate template) {
@@ -19,7 +25,8 @@ public class WebSocketEventPublisher extends EventPublisher {
     @Override
     public void receive(CEPEvent[] events) {
 
-        for (final CEPEvent cepEvent : events)
-            template.convertAndSend("/v1/topic", cepEvent.toString());
+        for (final CEPEvent cepEvent : events) {
+            template.convertAndSend("/v1/topic", String.format("%s - %s: %s", TIMESTAMP_FORMAT.print(new DateTime(cepEvent.getTimeStamp())), cepEvent.getStreamId(), Arrays.toString(cepEvent.getData())));
+        }
     }
 }
---
+++ smalldata-cep-publish-ws/src/test/java/uk/co/foyst/smalldata/cep/publisher/ws/WebSocketEventPublisherTests.java
@@ -0,0 +1,43 @@
+package uk.co.foyst.smalldata.cep.publisher.ws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import uk.co.foyst.smalldata.cep.CEPEvent;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WebSocketEventPublisherTests {
+
+    @Mock
+    private SimpMessagingTemplate messagingTemplate;
+
+    @InjectMocks
+    private WebSocketEventPublisher webSocketEventPublisher;
+
+    @Test
+    public void shouldStringifyCepEventWhenPassingToClient() {
+
+        // Arrange
+        CEPEvent event = new CEPEvent("TestStream", 12345, new Object[]{"John", true, 321});
+        CEPEvent[] events = new CEPEvent[]{event};
+
+        // Act
+        webSocketEventPublisher.receive(events);
+
+        // Assert
+        final ArgumentCaptor<Object> objectArgumentCaptor = ArgumentCaptor.forClass(Object.class);
+        verify(messagingTemplate, times(1)).convertAndSend(eq("/v1/topic"), objectArgumentCaptor.capture());
+        final String actualPayload = (String) objectArgumentCaptor.getValue();
+        final String expectedPayload = "1970-01-01 01:00:12,345 - TestStream: [John, true, 321]";
+        assertEquals(expectedPayload, actualPayload);
+    }
+}
